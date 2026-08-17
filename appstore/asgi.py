154e72d37dc5e73c90f44c7e12ff30011c3a1356
zere <zerekudarova@gmail.com>
2026-08-17 21:13:03 +0500

    create empty project

---
+++ appstore/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for appstore project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appstore.settings')
+
+application = get_asgi_application()
