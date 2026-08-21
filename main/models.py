8568897a62bbfa2a8eda709ed98e7c2c166ce269
zere <zerekudarova@gmail.com>
2026-08-21 21:07:52 +0500

    add app and category models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name= 'Категория'
+        verbose_name_plural= 'Категории'
+
+
+class App(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField(blank=True)
+    price=models.DecimalField(max_digits=6,decimal_places=2, default=0)
+    created_at=models.DateTimeField(auto_now_add=True)
+    category=models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Приложение'
+        verbose_name_plural = 'Приложения'
+
+
+
+
+
+
+
--- main/views.py
+++ main/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import App
 # Create your views here.
 
+# res=''
+# for i in App.objects.all():
+#     res+=f'{i.description}\n'
+
+
 def index(request):
-    return HttpResponse('Index here')
+    return HttpResponse(f'Приложений в магазине: {App.objects.count()}')
 
