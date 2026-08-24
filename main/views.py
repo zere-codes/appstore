2a648a5b68f9fa4d64bd5408f941ce1aee90889f
zere <zerekudarova@gmail.com>
2026-08-24 21:33:03 +0500

    static css, featured app

--- main/views.py
+++ main/views.py
@@ -3,11 +3,16 @@ from .models import App
 
 
 def index(request):
-    apps = App.objects.all()
+    apps = App.objects.order_by('name')
+
+    featured = App.objects.order_by('-price').first()
+
     return render(request, 'main/index.html', {
         'apps': apps,
         'apps_count': App.objects.count(),
+        'featured': featured,
     })
+
 
 def about(request):
     return render(request, 'main/about.html')
