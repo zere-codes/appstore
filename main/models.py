03112f27eb07e572c20b2eb215404987e70cfed8
zere <zerekudarova@gmail.com>
2026-09-04 21:43:34 +0500

    add paginstion, search, sort

--- main/models.py
+++ main/models.py
@@ -18,7 +18,7 @@ class App(models.Model):
     description=models.TextField(blank=True)
     price=models.DecimalField(max_digits=6,decimal_places=2, default=0)
     created_at=models.DateTimeField(auto_now_add=True)
-    category=models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    category=models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
--- main/views.py
+++ main/views.py
@@ -1,19 +1,39 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from .models import App, Category
 from django.http import HttpResponse
-
+from django.db.models import Q
+from django.core.paginator import Paginator
+SORTS = {
+    'new': '-created_at',
+    'name': 'name',
+    'price': 'price',
+    'expensive': '-price'
+}
 
 def index(request):
-    apps = App.objects.order_by('name')
+    q = request.GET.get('q', '')
+    sort = request.GET.get('sort', 'new')
 
+    if q:
+        apps = App.objects.filter(Q(name__icontains=q) | Q(description__icontains=q))
+    else:
+        apps = App.objects.all()
+
+    apps = apps.order_by(SORTS.get(sort, '-created_at'))
     featured = App.objects.order_by('-price').first()
-    category=Category.objects.all()
+    categories = Category.objects.all()
+
+    paginator = Paginator(apps, 3)
+    page_number=request.GET.get('page')
+    page_obj=paginator.get_page(page_number)
 
     return render(request, 'main/index.html', {
-        'apps': apps,
-        'apps_count': App.objects.count(),
+        'q': q,
+        'sort': sort,
+        'page_obj': page_obj,
         'featured': featured,
-        'categories': category,
+        'categories': categories,
     })
 
 
